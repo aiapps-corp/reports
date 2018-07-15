c76b7a222adf46e4a1b43a4ba55f1658dacb1bf1
Paulo R. Pereira de Souza <paulorpereirasouza@hotmail.com>
2018-07-15 19:32:04 -0300

    Add some code

---
+++ src/Aiapps.Reporting/IReportGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiapps.Reporting
+{
+    public interface IReportGenerator
+    {
+        byte[] Generate(string reportPath, byte[] logo, IDictionary<string, object[]> dataSources, string format, out string extension);
+        byte[] Generate(string reportPath, byte[] logo, IDictionary<string, string> paramenters, IDictionary<string, object[]> dataSources, string format, out string extension);
+        byte[] Generate(string reportPath, string logoPath, IDictionary<string, object[]> dataSources, string format, out string extension);
+        byte[] Generate(string reportPath, string logoPath, IDictionary<string, string> paramenters, IDictionary<string, object[]> dataSources, string format, out string extension);
+    }
+}
