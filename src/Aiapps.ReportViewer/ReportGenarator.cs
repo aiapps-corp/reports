63cf053dd41947845c0a422a5cf1ac2585726922
Paulo Renato Pereira de Souzaa <paulosouza@aiapps.com.br>
2023-08-19 06:47:41 -0300

    Remove Common.Logging

--- src/Aiapps.ReportViewer/ReportGenarator.cs
+++ src/Aiapps.ReportViewer/ReportGenarator.cs
@@ -1,19 +1,12 @@
 ﻿using Aiapps.Reporting;
-using Common.Logging;
 using Microsoft.Reporting.WebForms;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aiapps.ReportViewer
 {
     public class ReportGenarator : IReportGenerator
     {
-        private ILog _logger = LogManager.GetLogger<ReportGenarator>();
-
         public byte[] Generate(string reportPath, string logoPath, IDictionary<string, object[]> dataSources, string format, out string extension)
         {
             return Generate(reportPath,
@@ -63,14 +56,11 @@ namespace Aiapps.ReportViewer
                 viewer.ProcessingMode = ProcessingMode.Local;
                 viewer.LocalReport.EnableHyperlinks = true;
                 viewer.LocalReport.ReportPath = reportPath;
-                _logger.Debug($"Report Path: {reportPath}");
                 viewer.LocalReport.EnableExternalImages = true;
 
                 foreach (var d in paramenters)
                 {
-                    _logger.Debug($"Start SetParameters: {d.Key}");
                     viewer.LocalReport.SetParameters(new ReportParameter(d.Key, d.Value));
-                    _logger.Debug($"End SetParameters: {d.Key}");
                 }
 
                 ConfigureLogoParameter(logo, viewer);
@@ -80,16 +70,13 @@ namespace Aiapps.ReportViewer
                     var reportDataSource = new ReportDataSource(d.Key, d.Value);
                     viewer.LocalReport.DataSources.Add(reportDataSource);
                 }
-                _logger.Debug($"Start Render report: {reportPath}");
                 byte[] bytes = viewer.LocalReport.Render(
                         format, null, out mimeType, out encoding, out extension,
                         out streamids, out warnings);
-                _logger.Debug($"End Render report: {reportPath}");
                 return bytes;
             }
             catch (Exception ex)
             {
-                _logger.Error(ex.Message, ex);
                 throw;
             }
         }
@@ -98,7 +85,6 @@ namespace Aiapps.ReportViewer
         {
             if (!string.IsNullOrWhiteSpace(logo))
             {
-                _logger.Debug($"Start SetParameters ImagePath: {string.Join("", logo.Take(250))}");
                 if (logo.Contains("http"))
                 {
                     viewer.LocalReport.SetParameters(new ReportParameter("ImagePath", logo));
@@ -107,7 +93,6 @@ namespace Aiapps.ReportViewer
                 {
                     viewer.LocalReport.SetParameters(new ReportParameter("ReportLogo", logo));
                 }
-                _logger.Debug($"End SetParameters ImagePath: {string.Join("", logo.Take(250))}");
             }
         }
     }
